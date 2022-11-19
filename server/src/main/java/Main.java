cf0c6bf3e099eca99cdc4368f46f55d8c037ab65
anshangPro <65436468+anshangPro@users.noreply.github.com>
2022-11-19 13:50:35 +0800

    homework: change indent

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,14 +2,14 @@ import Handler.GameHandler;
 import Handler.GameMatcher;
 
 public class Main {
-    private static final int THREAD = 2;
+  private static final int THREAD = 2;
 
-    public static void main(String[] args) {
-        Server server = Server.getInstance();
-        GameMatcher gameMatcher = GameMatcher.getInstance();
-        Thread serverThread = new Thread(server);
+  public static void main(String[] args) {
+    Server server = Server.getInstance();
+    GameMatcher gameMatcher = GameMatcher.getInstance();
+    Thread serverThread = new Thread(server);
 //        Thread gameMatcherThread = new Thread(gameMatcher);
-        serverThread.start();
+    serverThread.start();
 //        gameMatcherThread.start();
-    }
+  }
 }
--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -6,42 +6,42 @@ import java.net.Socket;
 
 public class Server implements Runnable {
 
-    private Server(){
+  private Server() {
 
+  }
+
+  private volatile static Server instance;
+
+  public static Server getInstance() {
+    if (instance == null) {
+      synchronized (Server.class) {
+        instance = new Server();
+      }
     }
+    return Server.instance;
+  }
 
-    private volatile static Server instance;
+  private static final int port = 11451;
 
-    public static Server getInstance(){
-        if (instance == null) {
-            synchronized (Server.class){
-                instance = new Server();
-            }
-        }
-        return Server.instance;
+  private boolean running = true;
+
+  public void stop() {
+    running = false;
+  }
+
+  @Override
+  public void run() {
+    try (ServerSocket server = new ServerSocket(port)) {
+      while (running) {
+        Socket socket = server.accept();
+        System.out.printf("Client connected at port: %d\n", socket.getPort());
+        ClientHandler client = new ClientHandler(socket);
+        Thread clientThread = new Thread(client);
+        clientThread.start();
+      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
     }
-
-    private static final int port = 11451;
-
-    private boolean running = true;
-
-    public void stop() {
-        running = false;
-    }
-
-    @Override
-    public void run() {
-        try (ServerSocket server = new ServerSocket(port)) {
-            while (running) {
-                Socket socket = server.accept();
-                System.out.printf("Client connected at port: %d\n", socket.getPort());
-                ClientHandler client = new ClientHandler(socket);
-                Thread clientThread = new Thread(client);
-                clientThread.start();
-            }
-        } catch (IOException e){
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
+  }
 }
